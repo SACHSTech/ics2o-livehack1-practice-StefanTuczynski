672a3e1abb0d1dfce7f0ba6823025053e53bb16d
Stefan Valdemar Valdemar <stefanvaldemar.tuczynski23@ycdsbk12.ca>
2020-12-03 19:47:30 +0000

    Updated Version

--- days_hours.py
+++ days_hours.py
@@ -1,1 +1,12 @@
+"""
+Name: days_hours.py
+Purpose: Convert a set amount of days into hours.
 
+Author: Stefan Tuczynski
+
+Date: 03/12/2020
+"""
+
+#takes days given from user
+print("--- Welcome to the online days to hours converter ---")
+days int(input("Amount of days: "))
