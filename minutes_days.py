96d8f9b78b2b8acd611fbf57b6d965477ba43d02
Stefan Valdemar Valdemar <stefanvaldemar.tuczynski23@ycdsbk12.ca>
2020-12-03 20:01:48 +0000

    Updated Version

--- minutes_days.py
+++ minutes_days.py
@@ -1,1 +1,20 @@
+"""
+Name: minutes_days.py
+Purpose: Convert a set amount of minutes into days.
+
+Author: Stefan Tuczynski
+
+Date: 03/12/2020
+"""
+
+#takes minutes given from user
+print("--- Welcome to the online minutes to days converter ---")
+minutes = int(input("Amount of minutes: "))
+
+#processing data given from user 
+days = (minutes / 1440)
+
+#outputting amount of days
+print(minutes,"minutes equals:",days,"days")
+print("--- Thank you for using the online minutes to days converter! ---")
 
