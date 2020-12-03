cb9d65482ca2e20e48c24877f74ad6e7b804a16c
Stefan Valdemar Valdemar <stefanvaldemar.tuczynski23@ycdsbk12.ca>
2020-12-03 19:54:28 +0000

    Updated Version

--- days_hours.py
+++ days_hours.py
@@ -9,4 +9,12 @@ Date: 03/12/2020
 
 #takes days given from user
 print("--- Welcome to the online days to hours converter ---")
-days int(input("Amount of days: "))
+days = int(input("Amount of days: "))
+
+#processing data given from user 
+hours = (days * 24)
+
+#outputting amount of hours
+print(days,"days equals:",hours,"hours")
+print("--- Thank you for using the online days to hours converter! ---")
+print("--- Please consider donating ;D ---")
